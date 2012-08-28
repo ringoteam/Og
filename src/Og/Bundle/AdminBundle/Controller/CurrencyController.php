49f0ec3446ae9da10afa268826aa7f59b3256f6f
itkg-cguinet <ubuntu@ubuntu-VirtualBox>
2012-08-28 10:17:54 +0200

    ajout des controller

---
+++ src/Og/Bundle/AdminBundle/Controller/CurrencyController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Og\Bundle\AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sonata\AdminBundle\Controller\CRUDController;
+
+use PHPPdf\Autoloader;
+use PHPPdf\Core\FacadeBuilder;
+use PHPPdf\DataSource\DataSource;
+
+class CurrencyController extends CRUDController
+{
+    
+}
